ecc1fef5b44c6a763bd1e904d60a1841dcae3713
Kate Nguyen <kate.vy.nguyen@gmail.com>
2015-07-21 12:26:37 -0400

    Initial

---
+++ factorsample.php
@@ -0,0 +1,49 @@
+<?php include 'nav.php'; ?>
+<div class="container">
+<div class="jumbotron" id="volleyball">
+  <h1 style="padding-top:50px">Sport Culture</h1>
+  
+</div>
+</div>
+
+
+<div class="other">
+<div class="container-fluid" style="padding-top:10px;">
+	<center><img width="900px" src="img/dashboard.png"></center>
+</div>
+
+<div class="container-fluid" style="padding-top:50px;">
+	<div class="row">
+	<div class="col-md-6 col-md-offset-3">
+		<div class="panel panel-primary myPan">
+			<!-- Default panel contents -->
+			  <div class="panel-heading">Panel heading</div>
+			  <div class="panel-body">
+			    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet elementum libero quis volutpat. Aliquam molestie erat eu imperdiet facilisis. Vestibulum eu ante dui. Nullam dolor nibh, ultrices vitae tempor a, dapibus eget erat. Fusce sed augue tempus, tincidunt lectus a, ullamcorper lectus.</p>
+			  </div>
+
+			  <!-- Table -->
+			  <table class="table table-bordered myTable">
+			  	<tr>
+			   		<th class="danger">Low</th>
+			   		<th class="warning">Medium</th>
+			   		<th class="success">High</th>
+
+			   	</tr>
+			   	<tr>
+			   		<td class="danger">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet elementum libero quis volutpat. Aliquam molestie erat eu imperdiet facilisis.</td>
+			   		<td class="warning">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet elementum libero quis volutpat. Aliquam molestie erat eu imperdiet facilisis.</td>
+			   		<td class="success">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet elementum libero quis volutpat. Aliquam molestie erat eu imperdiet facilisis.</td>
+			   	</tr>
+			   	<tr>
+			   		<td class="danger">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet elementum libero quis volutpat. Aliquam molestie erat eu imperdiet facilisis.</td>
+			   		<td class="warning">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet elementum libero quis volutpat. Aliquam molestie erat eu imperdiet facilisis.</td>
+			   		<td class="success">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet elementum libero quis volutpat. Aliquam molestie erat eu imperdiet facilisis.</td>
+			   	</tr>			   	
+			  </table>
+		</div>
+	</div>
+	</div>
+</div>
+</div>
+</div>
